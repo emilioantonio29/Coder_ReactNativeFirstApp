d7767b10b93f59e0a9e6b52b7931fd8eac7a0055
emmartinez <emilio_git@outlook.com>
2022-03-28 20:58:14 -0300

    desafio: juego de adivinar el numero part2

--- src/screens/gameScreen/index.js
+++ src/screens/gameScreen/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, View, TextInput, Button, TouchableWithoutFeedback, Keyboard, Platform, KeyboardAvoidingView} from 'react-native';
+import {Text, View, TextInput, Button, TouchableWithoutFeedback, Keyboard, Platform, KeyboardAvoidingView, Modal, Pressable} from 'react-native';
 import HeaderComponent from '../../components/header';
 import {styles} from './styles'
 import Card from '../../components/card'
@@ -18,8 +18,19 @@ const generateRandomBetween = (min, max, exclude) =>{
 }
 
 const GameScreen = ({userOptions, onStartGame}) =>{
-
+    const [modalVisible, setModalVisible] = React.useState(false);
     const [currentGuess, setCurrentGuess] = React.useState(generateRandomBetween(1,100,userOptions));
+    const [message, setMessage] = React.useState("");
+
+    const checkNumber = (type) =>{
+        setModalVisible(true)
+        if(type == "menor"){
+            currentGuess < userOptions ? setMessage(`¡FELICIDADES!, el numero random es menor al seleccionado (${currentGuess})`) : setMessage(`¡GAME OVER!. El numero random es mayor (${currentGuess})`)
+        }else{
+            currentGuess > userOptions ? setMessage(`¡FELICIDADES!, el numero random es mayor al seleccionado (${currentGuess})`) : setMessage(`¡GAME OVER!. El numero random es menor (${currentGuess})`)
+        }
+        
+    }
 
     console.warn("El numero random es: "+ currentGuess)
 
@@ -34,13 +45,13 @@ const GameScreen = ({userOptions, onStartGame}) =>{
                     <View style={{padding: 10}}>
                         <Button 
                             title='Menor'
-                            onPress={()=>{}}
+                            onPress={()=>{checkNumber("menor")}}
                             color='grey'
                         />
                         <Text></Text>
                         <Button
                             title='Mayor'
-                            onPress={()=>{}}
+                            onPress={()=>{checkNumber("mayor")}}
                             color='#0b5345'
                         />
                     </View>
@@ -56,8 +67,39 @@ const GameScreen = ({userOptions, onStartGame}) =>{
                     </View>
                 </Card>
             </View>
+            <View style={styles.centeredView}>
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={modalVisible}
+                    onRequestClose={() => {
+                    Alert.alert("Modal has been closed.");
+                    setModalVisible(!modalVisible);
+                    }}
+                >
+                    <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <Text style={styles.modalText}>{message}</Text>
+                        <View style={{padding: 10}}>
+                        <Text></Text>
+                            <Button
+                                title='Volver a Jugar'
+                                onPress={()=>{onStartGame(null)}}
+                                color='#0b5345'
+                            />
+                            <Button
+                                title='Cerrar modal'
+                                onPress={()=>{setModalVisible(false)}}
+                                color=''
+                            />
+                        </View>
+                    </View>
+                    </View>
+                </Modal>
+            </View>
         </>
     )
 }
+
 
 export default GameScreen;
